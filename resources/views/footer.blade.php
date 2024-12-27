46cb28c83ce843553c36f58eabfd72969ec01fcc
KepomPong2816 <indratama2816@gmail.com>
2024-12-27 22:33:47 +0700

    fix asset

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -74,17 +74,17 @@
 <div id="preloader"></div>
 
 <!-- Vendor JS Files -->
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
-<script src="assets/vendor/aos/aos.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-<script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="{{ asset('assets_guest/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets_guest/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{ asset('assets_guest/vendor/aos/aos.js') }}"></script>
+<script src="{{ asset('assets_guest/vendor/glightbox/js/glightbox.min.js') }}"></script>
+<script src="{{ asset('assets_guest/vendor/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{ asset('assets_guest/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+<script src="{{ asset('assets_guest/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets_guest/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
 
 <!-- Main JS File -->
-<script src="assets/js/main.js"></script>
+<script src="{{ asset('assets_guest/js/main.js') }}"></script>
 
 </body>
 
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Index - Impact Bootstrap Template</title>
+    <title>E - Learn</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
 
     <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ asset('assets_guest/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets_guest/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -20,22 +20,14 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="{{ asset('assets_guest/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_guest/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_guest/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_guest/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_guest/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
     <!-- Main CSS File -->
-    <link href="assets/css/main.css" rel="stylesheet">
-
-    <!-- =======================================================
-  * Template Name: Impact
-  * Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
-  * Updated: Aug 07 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+    <link href="{{ asset('assets_guest/css/main.css') }}" rel="stylesheet">
 </head>
 
 <body class="index-page">
@@ -61,22 +53,22 @@
         <div class="branding d-flex align-items-cente">
 
             <div class="container position-relative d-flex align-items-center justify-content-between">
-                <a href="index.html" class="logo d-flex align-items-center">
+                <a href="{{ url('/') }}" class="logo d-flex align-items-center">
                     <!-- Uncomment the line below if you also wish to use an image logo -->
-                    <!-- <img src="assets/img/logo.png" alt=""> -->
+                    <!-- <img src="assets_guest/img/logo.png" alt=""> -->
                     <h1 class="sitename">Impact</h1>
                     <span>.</span>
                 </a>
 
                 <nav id="navmenu" class="navmenu">
                     <ul>
-                        <li><a href="#hero" class="active">Home<br></a></li>
+                        {{-- <li><a href="#hero" class="active">Home<br></a></li> --}}
                         <li><a href="#about">About</a></li>
                         <li><a href="#services">Services</a></li>
                         <li><a href="#portfolio">Portfolio</a></li>
                         <li><a href="#team">Team</a></li>
                         <li><a href="blog.html">Blog</a></li>
-                        <li class="dropdown"><a href="#"><span>Dropdown</span> <i
+                        {{-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i
                                     class="bi bi-chevron-down toggle-dropdown"></i></a>
                             <ul>
                                 <li><a href="#">Dropdown 1</a></li>
@@ -94,8 +86,8 @@
                                 <li><a href="#">Dropdown 3</a></li>
                                 <li><a href="#">Dropdown 4</a></li>
                             </ul>
-                        </li>
-                        <li><a href="#contact">Contact</a></li>
+                        </li> --}}
+                        <li><a href="{{ url('login') }}"><button class="btn btn-primary">Login</button></a></li>
                     </ul>
                     <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
                 </nav>
